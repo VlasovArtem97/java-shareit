6ef0312dc925f19b95b5f1033128a2e96edba42f
VlasovArtem <Yukionn@yandex.ru>
2025-09-21 22:17:09 +0300

    18. fix#8 тесты

--- server/src/main/java/ru/practicum/shareit/exception/HandlerException.java
+++ server/src/main/java/ru/practicum/shareit/exception/HandlerException.java
@@ -1,16 +1,12 @@
 package ru.practicum.shareit.exception;
 
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
@@ -30,7 +26,7 @@ public class HandlerException {
         log.error("Ошибка в валидации - {}", e.getMessage());
         return Map.of("error", e.getMessage());
     }
-    
+
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
