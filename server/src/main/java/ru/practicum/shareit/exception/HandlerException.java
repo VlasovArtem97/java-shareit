bdb68778697cf49a4ded5e029d08dbb64e5c12a6
VlasovArtem <Yukionn@yandex.ru>
2025-09-22 01:00:35 +0300

    19. fix#9 тесты

--- server/src/main/java/ru/practicum/shareit/exception/HandlerException.java
+++ server/src/main/java/ru/practicum/shareit/exception/HandlerException.java
@@ -1,12 +1,16 @@
 package ru.practicum.shareit.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
@@ -27,6 +31,29 @@ public class HandlerException {
         return Map.of("error", e.getMessage());
     }
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handlerMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            errors.put("error", error.getDefaultMessage());
+        }
+        log.error("Ошибка валидации встроенных через @Valid - {}", errors);
+        return errors;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleConstraintViolation(final ConstraintViolationException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getConstraintViolations().forEach(violation -> {
+            String fieldName = violation.getPropertyPath().toString();
+            String errorMessage = violation.getMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        log.error("Ошибка валидации - {}", errors);
+        return errors;
+    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
