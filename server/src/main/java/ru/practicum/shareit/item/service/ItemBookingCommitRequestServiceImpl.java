4496d539b3f94e2f2732dcc3c973ea4fa831df79
VlasovArtem <Yukionn@yandex.ru>
2025-09-22 20:31:44 +0300

    23. fix#13 тесты

--- server/src/main/java/ru/practicum/shareit/item/service/ItemBookingCommitRequestServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemBookingCommitRequestServiceImpl.java
@@ -85,7 +85,7 @@ public class ItemBookingCommitRequestServiceImpl implements ItemBookingCommitReq
     public CommentDto addComment(Long userId, Long itemId, CommentDto newCommentDto) {
         User user = userService.returnUserFindById(userId);
         Item item = itemService.returnFindItemById(itemId);
-        List<Booking> bookings = bookingService.findBookingForComment(userId, itemId, newCommentDto.getCreated().minusSeconds(1));
+        List<Booking> bookings = bookingService.findBookingForComment(userId, itemId, newCommentDto.getCreated().minusSeconds(5));
         if (bookings.isEmpty()) {
             throw new IllegalStateException("Вы не можете оставлять комментарий, так как вы не бронировали данный item");
         }
