46235b5d2fe862eb4beddd81ebebf4682c45209b
VlasovArtem <Yukionn@yandex.ru>
2025-09-22 21:39:54 +0300

    22. fix#12 тесты

--- server/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -20,5 +20,5 @@ public class CommentDto {
     private String text;
     private String authorName;
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime created = ZonedDateTime.now(ZoneOffset.ofHours(3)).toLocalDateTime();
+    private LocalDateTime created = ZonedDateTime.now(ZoneOffset.ofHours(0)).toLocalDateTime();
 }
